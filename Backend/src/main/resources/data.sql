7602c1238c0d978c5a24a10f5f5e1047e2b28eaf
devpatel43543 <devpatel43543@gmail.com>
2024-10-19 18:56:36 -0300

    add local-mysql.yml

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- INSERT INTO category  VALUES (1, 'Electronics');
--- INSERT INTO category VALUES (2, 'Books');
--- INSERT INTO category  VALUES (3, 'Clothing');
--- INSERT INTO category VALUES (4, 'Home & Garden');
+INSERT INTO category  VALUES (1, 'Electronics');
+INSERT INTO category VALUES (2, 'Books');
+INSERT INTO category  VALUES (3, 'Clothing');
+INSERT INTO category VALUES (4, 'Home & Garden');
