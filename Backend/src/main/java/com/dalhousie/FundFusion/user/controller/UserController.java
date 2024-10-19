992fa30d90c2f0c67fa417950012cba50d0430f8
Sivarajesh Balamurali <b.sivarajesh20@gmail.com>
2024-10-19 18:22:56 -0300

    bug: Updating CI-CD

--- Backend/src/main/java/com/dalhousie/FundFusion/user/controller/UserController.java
+++ Backend/src/main/java/com/dalhousie/FundFusion/user/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.dalhousie.FundFusion.user.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/check")
 public class UserController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+
 }
