e7ea8f10ec724f43d3e130cda583be0ec51754ab
Sivarajesh Balamurali <b.sivarajesh20@gmail.com>
2024-10-20 13:03:11 -0300

    bug: Adding User to UserTransaction

--- Backend/src/main/java/com/dalhousie/FundFusion/user/entity/UserTransaction.java
+++ Backend/src/main/java/com/dalhousie/FundFusion/user/entity/UserTransaction.java
@@ -19,7 +19,11 @@ public class UserTransaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer txnId;
+    @OneToOne
+    @JoinColumn(name = "userId")
+    private User user;
     private String txnDesc;
+    @NotNull(message = "Expense cannot be empty")
     private Float expense;
     private LocalDate txnDate;
     @ManyToOne
