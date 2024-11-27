4c27e0137c1c9adb7db88d62d3f31870c7cf7e42
Spidey0819 <goswamidhruv302031@gmail.com>
2024-11-27 15:06:28 -0400

    user can now add their expense realated to groups

---
+++ Frontend/src/utils/Category.js
@@ -0,0 +1,18 @@
+export const catrgory = {
+    "1": "Food",
+    "2": "Housing",
+    "3": "Transportation",
+    "4": "Travel",
+    "5": "Entertainment",
+    "6": "Gifts",
+    "7": "Health",
+    "8": "Education",
+    "9": "Shopping",
+    "10": "Household",
+    "11": "Events",
+    "12": "Personal",
+    "13": "Childcare",
+    "14": "Miscellaneous",
+    "15": "Debt",
+    "16": "Insurance"
+}
